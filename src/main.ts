386d2265ed63bf4607a8b0425fcc8885cb330414
Monika Khanka <m.khanka@andersenlab.com>
2025-09-27 16:58:24 +0530

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://quiz-builder-app-five.vercel.app:3001'],
+    origin: ['https://quiz-builder-app-five.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
